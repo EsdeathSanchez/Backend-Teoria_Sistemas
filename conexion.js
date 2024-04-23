c2c613738f7b2088d77ca7ed61f35ce920bd0b68
Edwin Sanchez <sanchezplayer15@gmail.com>
2024-04-23 00:17:24 -0600

    conexion a la base de datos exitosa, v 1.0.1

--- conexion.js
+++ conexion.js
@@ -17,25 +17,19 @@ const db = mysql.createPool({
   user: config.database.user,
   password: config.database.password,
   database: config.database.database,
-  //connectTimeout: 60000, // Tiempo de espera de conexión en milisegundos (60 segundos)
-  //waitForConnections: true,
-  //connectionLimit: 10,
-  //queueLimit: 0,
-  //Promise: global.Promise  //habilitar promesas
 });
 
 
-app.get('/obtenerDatosORG', (req, res) => {
-    try {
+app.get('/obtenerDatosORG', async (req, res) => {
+  try {
       const sqlConsulta = 'SELECT * FROM USUARIO';
-      const resultados = db.query(sqlConsulta);
-      logger.info('Datos consultados desde la base de datos de la tabla org:', resultados[0]);
-      res.status(200).json({ success: true, data: resultados[0] });
-    } catch (error) {
-      logger.error('Error al obtener datos de la tabla org:', error);
+      const [resultados, campos] = await db.query(sqlConsulta);
+      res.status(200).json({ success: true, data: resultados });
+  } catch (error) {
       res.status(500).json({ success: false, message: 'Error al obtener datos de la tabla' });
-    }
-  });
+  }
+});
+
 
 app.listen(port, () => {
     console.log(`Servidor en ejecución en http://localhost:${port}`);
